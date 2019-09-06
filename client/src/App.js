f23cccf7e36e89882b0898f2574c13ecdec3b01b
Unknown <luke.towell@hotmail.com>
2019-09-06 16:32:23 +0100

    All of the setup for the Fib and Other page components
    - All of the set up for the fib and other page components
    - added in the logic for the relevant components to display correctly.
    - wired up the routing of the two pages.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
+import Fib from './Fib';
+import OtherPage from './OtherPage';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div className="App">
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div>
+          <Route exact path="/" component={Fib}/>
+          <Route path="/otherpage" component={OtherPage}/>
+        </div>
+      </Router>
     );
   }
 }
