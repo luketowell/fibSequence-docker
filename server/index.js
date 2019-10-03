9543c4307eefa5fa8a73e237463504a1b49252a4
Luke Towell <luke.towell@hotmail.com>
2019-10-03 22:10:06 +0100

    Complete setup and nginx setup
    finished set up and configure so that application definitely works

--- server/index.js
+++ server/index.js
@@ -1,74 +1,74 @@
-const keys = require('./keys');
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const keys = require("./keys");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 // Express app setup
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.status(200).send('pong')
-})
-
-app.get('/ping', (req, res) => {
-    res.status(200).send('pong')
-})
-
-app.get('/values/all', async (req, res) => {
-    const values = await pgClient.query('SELECT * from values');
-
-    res.status(200).send(values.rows)
-})
-
-app.get('/values/current', async (req, res) => {
-    redisClient.hgetall('values', (err, values) => {
-        res.send(values)
-    });
-})
-
-app.post('/values', async (req, res) => {
-    const index = req.body.index;
-
-    if (parseInt(index) > 40) {
-        return res.status(422).send('Index is too high');
-    }
-    else{
-    redisClient.hset('values', index, 'Nothing yet!');
-    redisPublisher.publish('insert', index);
-    pgClient.query('INSERT INTO values(number) VALUES ($1)', [index])
-
-    res.send({working:true});
-    }
-})
-
 app.listen(5000, () => {
-    console.log('app listening on port 5000');
-})
+  console.log("app listening on port 5000");
+});
 
 //PostGres setup
-const { Pool } = require ('pg');
+const { Pool } = require("pg");
 const pgClient = new Pool({
-    user: keys.pgUser,
-    host: keys.pgHost,
-    database: keys.pgDatabase,
-    password: keys.pgPassword,
-    port: keys.pgPort
-})
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort
+});
 
-pgClient.on('error', err => console.log('lost connection to postgres client'))
+pgClient.on("error", err => console.log("lost connection to postgres client"));
 
-pgClient.query('CREATE TABE IF NOT EXISTS values (number INT)')
-.catch(err => console.log(err));
+pgClient
+  .query("CREATE TABLE IF NOT EXISTS values (number INT)")
+  .catch(err => console.log(err));
 
 // redis strategy
 
-const redis = require('redis');
+const redis = require("redis");
 const redisClient = redis.createClient({
-    host: keys.redisHost,
-    port: keys.redisPort,
-    retry_strategy: () => 1000
-})
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
 
 const redisPublisher = redisClient.duplicate();
+
+app.get("/", (req, res) => {
+  res.status(200).send("pong");
+});
+
+app.get("/ping", (req, res) => {
+  res.status(200).send("pong");
+});
+
+app.get("/values/all", async (req, res) => {
+  const values = await pgClient.query("SELECT * from values");
+
+  res.status(200).send(values.rows);
+});
+
+app.get("/values/current", async (req, res) => {
+  redisClient.hgetall("values", (err, values) => {
+    res.send(values);
+  });
+});
+
+app.post("/values", async (req, res) => {
+  const index = req.body.index;
+
+  if (parseInt(index) > 40) {
+    return res.status(422).send("Index is too high");
+  } else {
+    redisClient.hset("values", index, "Nothing yet!");
+    redisPublisher.publish("insert", index);
+    pgClient.query("INSERT INTO values(number) VALUES ($1)", [index]);
+
+    res.send({ working: true });
+  }
+});
--- worker/index.js
+++ worker/index.js
@@ -4,18 +4,20 @@ const fib = require("./fib");
 
 //create the redis client with the provided connection params
 const redisClient = redis.createClient({
-    host: keys.redisHost,
-    port: keys.redisPort,
-    retry_strategy: () => 1000
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
 });
+
+console.log(keys.redisHost, keys.redisPort);
 
 //subscribes to the redisclient that has been created above
 const sub = redisClient.duplicate();
 
 /*from the subscribe when a new message is received we want to add the
  new value into the redis db with the value and the worked out fib sequence.*/
-sub.on('message', (channel, message) => {
-    redisClient.hset('values', message, fib.fib(parseInt(message)));
+sub.on("message", (channel, message) => {
+  redisClient.hset("values", message, fib.fib(parseInt(message)));
 });
 
-sub.subscribe('insert');
+sub.subscribe("insert");
